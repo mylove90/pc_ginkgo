1982bf6c9e8a37d3d4b1165b88fba7e6e2405e7e
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2019-06-19 16:40:17 -0700

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I43dbbbd99674c79949bbdc4f78588362e772302f
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    [render: minor fixups, account for feec() returning -1, and
    start_cpu in fbt()]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -2538,17 +2538,21 @@ void update_best_cluster(struct related_thread_group *grp,
 {
 	if (boost) {
 		grp->preferred_cluster = sched_cluster[1];
+		grp->skip_min = true;
 		return;
 	}
 
 	if (grp->preferred_cluster == sched_cluster[0]) {
-		if (demand >= sched_group_upmigrate)
+		if (demand >= sched_group_upmigrate) {
 			grp->preferred_cluster = sched_cluster[1];
+			grp->skip_min = true;
+		}
 		return;
 	}
 	if (demand < sched_group_downmigrate) {
 		if (!sysctl_sched_coloc_downmigrate_ns) {
 			grp->preferred_cluster = sched_cluster[0];
+			grp->skip_min = false;
 			return;
 		}
 		if (!grp->downmigrate_ts) {
@@ -2559,6 +2563,7 @@ void update_best_cluster(struct related_thread_group *grp,
 				sysctl_sched_coloc_downmigrate_ns) {
 			grp->preferred_cluster = sched_cluster[0];
 			grp->downmigrate_ts = 0;
+			grp->skip_min = false;
 		}
 	} else if (grp->downmigrate_ts)
 		grp->downmigrate_ts = 0;
@@ -2592,6 +2597,7 @@ static void _set_preferred_cluster(struct related_thread_group *grp)
 
 	if (!hmp_capable()) {
 		grp->preferred_cluster = sched_cluster[0];
+		grp->skip_min = false;
 		return;
 	}
 
