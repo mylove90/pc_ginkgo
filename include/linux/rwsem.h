d8209f879755d7c0f15ab51ac0b9f1798b93a84e
Waiman Long <longman@redhat.com>
2020-11-20 23:14:13 -0500

    BACKPORT: locking/rwsem: Prevent potential lock starvation
    The lock handoff bit is added in commit 4f23dbc1e657 ("locking/rwsem:
    Implement lock handoff to prevent lock starvation") to avoid lock
    starvation. However, allowing readers to do optimistic spinning does
    introduce an unlikely scenario where lock starvation can happen.
    
    The lock handoff bit may only be set when a waiter is being woken up.
    In the case of reader unlock, wakeup happens only when the reader count
    reaches 0. If there is a continuous stream of incoming readers acquiring
    read lock via optimistic spinning, it is possible that the reader count
    may never reach 0 and so the handoff bit will never be asserted.
    
    One way to prevent this scenario from happening is to disallow optimistic
    spinning if the rwsem is currently owned by readers. If the previous
    or current owner is a writer, optimistic spinning will be allowed.
    
    If the previous owner is a reader but the reader count has reached 0
    before, a wakeup should have been issued. So the handoff mechanism
    will be kicked in to prevent lock starvation. As a result, it should
    be OK to do optimistic spinning in this case.
    
    This patch may have some impact on reader performance as it reduces
    reader optimistic spinning especially if the lock critical sections
    are short the number of contending readers are small.
    
    Change-Id: Icfaea5ade20c08744029040946c7e73e5b13d506
    Signed-off-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Davidlohr Bueso <dbueso@suse.de>
    Link: https://lkml.kernel.org/r/20201121041416.12285-3-longman@redhat.com
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- include/linux/rwsem.h
+++ include/linux/rwsem.h
@@ -48,6 +48,10 @@ struct rw_semaphore {
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map	dep_map;
 #endif
+#ifdef CONFIG_RWSEM_PRIO_AWARE
+	/* count for waiters preempt to queue in wait list */
+	long m_count;
+#endif
 };
 
 /*
