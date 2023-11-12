mylove90/ginkgo_caf

Add drivers/ modifications
Signed-off-by: mylove90 <engm.saad1990@gmail.com>

drivers/irqchip/irq-gic-v3.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013-2017 ARM Limited, All Rights Reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Author: Marc Zyngier <marc.zyngier@arm.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -43,6 +44,7 @@
 #include <linux/syscore_ops.h>
 #include <linux/suspend.h>
 #include <linux/notifier.h>
+#include <linux/wakeup_reason.h> /*Add-HMI_M516_A01-51*/
 
 #include "irq-gic-common.h"
 
@@ -464,6 +466,9 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = desc->action->name;
 
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+
+		log_wakeup_reason(irq); /*Add-HMI_M516_A01-51*/
+
 	}
 }
 

mylove90/pc_ginkgo

drivers/irqchip/irq-gic-v3.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013-2017 ARM Limited, All Rights Reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Author: Marc Zyngier <marc.zyngier@arm.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -44,6 +45,7 @@
 #include <linux/syscore_ops.h>
 #include <linux/suspend.h>
 #include <linux/notifier.h>
+#include <linux/wakeup_reason.h> /*Add-HMI_M516_A01-51*/
 
 #include "irq-gic-common.h"
 
@@ -471,6 +473,9 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = desc->action->name;
 
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+
+		log_irq_wakeup_reason(irq); /*Add-HMI_M516_A01-51*/
+
 	}
 }
 